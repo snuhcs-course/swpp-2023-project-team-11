f333a04c0576fad52d13c61be700c8d0e368395c
sangin514 <sangin514@naver.com>
2023-10-14 16:32:36 +0900

    feat: add profile, food, movie, hobby, user_hobby db models

--- fastapi/src/user/models.py
+++ fastapi/src/user/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy import Column, Integer, String, BigInteger, Date, ForeignKey
 
 from src.database import Base
 
@@ -6,7 +6,7 @@ from src.database import Base
 class Email(Base):
     __tablename__ = "email"
 
-    email = Column(String, nullable=False, unique=True)
+    email = Column(String(31), nullable=False, unique=True)
     email_id = Column(BigInteger, primary_key=True)
 
 
@@ -22,3 +22,40 @@ class VerifiedEmail(Base):
 
     email_id = Column(BigInteger, nullable=False, unique=True, ForeignKey=Email.email_id)
     verification_id = Column(BigInteger, primary_key=True)
+
+
+class Profile(Base):
+    __tablename__ = "profile"
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    birth = Column(Date, nullable=False)
+    sex = Column(String(1), nullable=False)
+    major = Column(String(63), nullable=False)
+    admission_year = Column(Integer, nullable=False)
+    about_me = Column(String(255))
+    mbti = Column(Integer)
+    favorite_food = Column(Integer, ForeignKey("food.food_id"))
+    favorite_movie = Column(Integer, ForeignKey("movie.movie_id"))
+
+class Food(Base):
+    __tablename__ = "food"
+    food_id = Column(Integer, primary_key=True)
+    food = Column(String(31), nullable=False, unique=True)
+
+
+class Movie(Base):
+    __tablename__ = "movie"
+    movie_id = Column(Integer, primary_key=True)
+    movie_name = Column(String(63), nullable=False, unique=True)
+
+
+class Hobby(Base):
+    __tablename__ = "hobby"
+    hobby_id = Column(Integer, primary_key=True)
+    hobby = Column(String(31), nullable=False, unique=True)
+
+
+class UserHobby(Base):
+    __tablename__ = "user_hobby"
+    user_id = Column(Integer, ForeignKey("profile.user_id"))
+    hobby_id = Column(Integer, ForeignKey("hobby.hobby_id"))
