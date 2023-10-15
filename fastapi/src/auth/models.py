255fbb07389ef494ba299f6ee137c53ec5204684
sangin514 <sangin514@naver.com>
2023-10-15 23:21:52 +0900

    fix: add autoincrement in id field

--- fastapi/src/auth/models.py
+++ fastapi/src/auth/models.py
@@ -8,7 +8,7 @@ from src.user.models import User
 class Session(Base):
     __tablename__ = "session"
 
-    id: Mapped[int] = Column(Integer, primary_key=True)
+    id: Mapped[int] = Column(Integer, primary_key=True, autoincrement=True)
     session_key: Mapped[str] = Column(String(44), nullable=False, unique=True)
     user_id: Mapped[int] = Column(ForeignKey("users.user_id"), nullable=False)
     
