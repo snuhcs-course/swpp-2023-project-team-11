62f32035c5648e372ee9f35e99545d2a9e3870ed
sangin514 <sangin514@naver.com>
2023-10-15 22:51:43 +0900

    fix: revise src/database.py: implements get_db and revise the implementation

--- fastapi/src/database.py
+++ fastapi/src/database.py
@@ -1,13 +1,26 @@
-from sqlalchemy import Engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.orm.decl_api import DeclarativeMeta
+import os
 
+
+PG_DB = os.environ.get('SNEK_POSTGRES_DB')
+PG_USER = os.environ.get('SNEK_POSTGRES_USER')
+PG_PW = os.environ.get('SNEK_POSTGRES_PW')
+TEST_DB_URL: str = f"postgresql://{PG_USER}:{PG_PW}@localhost:5432/{PG_DB}"
+
+
+engine: Engine = create_engine(TEST_DB_URL)
+SessionLocal: sessionmaker[Session] = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base Model Class
 Base: DeclarativeMeta = declarative_base()
 
 
-# Session Class
-def declarative_session(engine: Engine) -> sessionmaker[Session]:
-    return sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
