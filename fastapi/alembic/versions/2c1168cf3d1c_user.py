7b14848b1f32d04ebdc23913a90c71b0052fbe07
sangin514 <sangin514@naver.com>
2023-10-22 18:43:46 +0900

    fix: Combine user, category_data alembic file

--- fastapi/alembic/versions/2c1168cf3d1c_user.py
+++ fastapi/alembic/versions/2c1168cf3d1c_user.py
@@ -126,7 +126,102 @@ def upgrade() -> None:
     sa.ForeignKeyConstraint(['lang_id'], ['language.id'], ondelete='CASCADE'),
     sa.ForeignKeyConstraint(['user_id'], ['users.user_id'], ondelete='CASCADE')
     )
-    # ### end Alembic commands ###
+
+
+    # Insert category data(lang, food, hobby, location, movie)
+    meta = sa.MetaData()
+    meta.reflect(only=('language', 'food', 'hobby', 'location', 'movie'), bind=op.get_bind())
+
+    language_data = [{'name': 'english'},
+                     {'name': 'spanish'},
+                     {'name': 'chinese'},
+                     {'name': 'arabic'},
+                     {'name': 'french'},
+                     {'name': 'german'},
+                     {'name': 'japanese'},
+                     {'name': 'russian'},
+                     {'name': 'portuguese'},
+                     {'name': 'korean'},
+                     {'name': 'italian'},
+                     {'name': 'dutch'},
+                     {'name': 'swedish'},
+                     {'name': 'turkish'},
+                     {'name': 'hebrew'},
+                     {'name': 'hindi'},
+                     {'name': 'thai'},
+                     {'name': 'greek'},
+                     {'name': 'vietnamese'},
+                     {'name': 'finnish'},
+                     ]
+    food_data = [{'name': 'korean'}, {'name': 'spanish'}, {'name': 'american'},
+                 {'name': 'italian'}, {'name': 'thai'}, {'name': 'chinese'},
+                 {'name': 'japanese'}, {'name': 'indian'}, {'name': 'mexican'},
+                 {'name': 'vegan'}, {'name': 'dessert'}]
+    hobby_data = [
+        {'name': 'painting'},
+        {'name': 'gardening'},
+        {'name': 'hiking'},
+        {'name': 'reading'},
+        {'name': 'cooking'},
+        {'name': 'photography'},
+        {'name': 'dancing'},
+        {'name': 'swimming'},
+        {'name': 'cycling'},
+        {'name': 'traveling'},
+        {'name': 'gaming'},
+        {'name': 'fishing'},
+        {'name': 'knitting'},
+        {'name': 'music'},
+        {'name': 'yoga'},
+        {'name': 'writing'},
+        {'name': 'shopping'},
+        {'name': 'teamSports'},
+        {'name': 'fitness'},
+        {'name': 'movie'}
+    ]
+    movie_data = [
+        {'name': 'action'},
+        {'name': 'adventure'},
+        {'name': 'animation'},
+        {'name': 'comedy'},
+        {'name': 'drama'},
+        {'name': 'fantasy'},
+        {'name': 'horror'},
+        {'name': 'mystery'},
+        {'name': 'romance'},
+        {'name': 'scienceFiction'},
+        {'name': 'thriller'},
+        {'name': 'western'}
+    ]
+    location_data = [
+        {'name': 'humanity'},
+        {'name': 'naturalScience'},
+        {'name': 'dormitory'},
+        {'name': 'socialScience'},
+        {'name': 'humanEcology'},
+        {'name': 'agriculture'},
+        {'name': 'highEngineering'},
+        {'name': 'lowEngineering'},
+        {'name': 'business'},
+        {'name': 'jahayeon'},
+        {'name': 'studentUnion'},
+        {'name': 'seolYeep'},
+        {'name': 'nockDoo'},
+        {'name': 'bongcheon'}
+    ]
+    language_table = sa.Table('language', meta)
+    op.bulk_insert(language_table, language_data)
+    food_table = sa.Table('food', meta)
+    op.bulk_insert(food_table, food_data)
+    hobby_table = sa.Table('hobby', meta)
+    op.bulk_insert(hobby_table, hobby_data)
+    movie_table = sa.Table('movie', meta)
+    op.bulk_insert(movie_table, movie_data)
+    location_table = sa.Table('location', meta)
+    op.bulk_insert(location_table, location_data)
+
+
+# ### end Alembic commands ###
 
 
 def downgrade() -> None:
