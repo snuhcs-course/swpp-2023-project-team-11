82b22201640d157806827d327f2407db1987302f
Kangji <drcotr0623@naver.com>
2023-10-29 04:56:45 +0900

    test: add unit test for chatting API

--- fastapi/src/chatting/service.py
+++ fastapi/src/chatting/service.py
@@ -1,5 +1,5 @@
 from typing import List
-from sqlalchemy import insert, update, desc
+from sqlalchemy import insert, update, desc, or_
 from sqlalchemy.orm import Session as DbSession
 
 from src.chatting.exceptions import *
@@ -8,11 +8,8 @@ from src.chatting.models import *
 
 def get_all_chattings(user_id: int, is_approved: bool, db: DbSession) -> List[Chatting]:
     return db.query(Chatting).where(
-            Chatting.initiator_id == user_id or Chatting.responder_id == user_id
-        ).where(
-            Chatting.is_approved == is_approved
-        ).order_by(
-            desc(Chatting.created_at)).all()
+            or_(Chatting.initiator_id == user_id, Chatting.responder_id == user_id)
+        ).where(Chatting.is_approved == is_approved).order_by(desc(Chatting.created_at)).all()
 
 
 def create_chatting(user_id: int, responder_id: int, db: DbSession) -> Chatting:
@@ -24,7 +21,7 @@ def create_chatting(user_id: int, responder_id: int, db: DbSession) -> Chatting:
 
 
 def approve_chatting(user_id: int, chatting_id: int, db: DbSession) -> Chatting:
-    chatting = db.scalar(update(Chatting).values(is_approved=True).where(Chatting.id == chatting_id).where(Chatting.responder_id == user_id))
+    chatting = db.scalar(update(Chatting).values(is_approved=True).where(Chatting.id == chatting_id).where(Chatting.responder_id == user_id).returning(Chatting))
     if chatting is None:
         raise InvalidChattingException()
     
@@ -32,7 +29,7 @@ def approve_chatting(user_id: int, chatting_id: int, db: DbSession) -> Chatting:
 
 
 def terminate_chatting(user_id: int, chatting_id: int, db: DbSession) -> Chatting:
-    chatting = db.scalar(update(Chatting).values(is_terminated=True).where(Chatting.id == chatting_id).where(Chatting.responder_id == user_id or Chatting.initiator_id == user_id))
+    chatting = db.scalar(update(Chatting).values(is_terminated=True).where(Chatting.id == chatting_id).where(or_(Chatting.responder_id == user_id, Chatting.initiator_id == user_id)).returning(Chatting))
     if chatting is None:
         raise InvalidChattingException()
     
@@ -40,7 +37,7 @@ def terminate_chatting(user_id: int, chatting_id: int, db: DbSession) -> Chattin
 
 
 def get_all_texts(user_id: int, chatting_id: int | None, seq_id: int, limit: int | None, db: DbSession) -> List[Text]:
-    query = db.query(Text).join(Text.chatting).where(Chatting.initiator_id == user_id or Chatting.responder_id == user_id).where(Text.id > seq_id).order_by(desc(Text.id))
+    query = db.query(Text).join(Text.chatting).where(or_(Chatting.initiator_id == user_id, Chatting.responder_id == user_id)).where(Text.id > seq_id).order_by(desc(Text.id))
     if chatting_id is not None:
         query = query.where(Chatting.id == chatting_id)
     if limit is not None:
--- fastapi/tests/chatting.py
+++ fastapi/tests/chatting.py
@@ -1,9 +1,124 @@
+from datetime import timedelta
 import unittest
 
+from sqlalchemy import insert, delete, select
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+from src.database import Base, DbConnector
+from src.chatting.dependencies import *
+from src.chatting.models import *
+from src.chatting.service import *
+from tests.utils import *
+
+
+class TestDependencies(unittest.TestCase):
+    email = "test@snu.ac.kr"
+    invalid = "hello@snu.ac.kr"
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        Base.metadata.create_all(bind=DbConnector.engine)
+        for db in DbConnector.get_db():
+            cls.profile_id = setup_user(db, cls.email)
+            db.commit()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        for db in DbConnector.get_db():
+            teardown_user(db)
+            db.commit()
+
+    def test_get_user_id(self) -> None:
+        valid_req = CreateChattingRequest(counterpart=self.email)
+        invalid_req = CreateChattingRequest(counterpart=self.invalid)
+
+        for db in DbConnector.get_db():
+            self.assertEqual(self.profile_id, get_user_id(valid_req, db))
+            with self.assertRaises(InvalidUserException):
+                get_user_id(invalid_req, db)
+
+
+class TestService(unittest.TestCase):
+    initiator = "test@snu.ac.kr"
+    responder = "hello@snu.ac.kr"
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        Base.metadata.create_all(bind=DbConnector.engine)
+        for db in DbConnector.get_db():
+            cls.initiator_id = setup_user(db, cls.initiator)
+            cls.responder_id = setup_user(db, cls.responder)
+            db.commit()
+    
+    @classmethod
+    def tearDownClass(cls) -> None:
+        for db in DbConnector.get_db():
+            teardown_user(db)
+            db.commit()
+
+    def tearDown(self) -> None:
+        for db in DbConnector.get_db():
+            db.execute(delete(Text))
+            db.execute(delete(Chatting))
+            db.commit()
+
+    def test_chatting(self):
+        for db in DbConnector.get_db():
+            chatting = create_chatting(self.initiator_id, self.responder_id, db)
+            chatting_id = chatting.id
+            self.assertIsNotNone(chatting)
+            self.assertEqual(chatting.is_approved, False)
+            self.assertEqual(chatting.is_terminated, False)
+            db.commit()
+
+            self.assertEqual(get_all_chattings(self.initiator_id, True, db), [])
+            self.assertEqual(len(get_all_chattings(self.initiator_id, False, db)), 1)
+            self.assertEqual(len(get_all_chattings(self.responder_id, False, db)), 1)
+
+            with self.assertRaises(InvalidChattingException):
+                approve_chatting(self.initiator_id, chatting_id, db)
+            with self.assertRaises(InvalidChattingException):
+                approve_chatting(self.responder_id, -1, db)
+            chatting = approve_chatting(self.responder_id, chatting_id, db)
+            db.commit()
+
+            self.assertEqual(chatting.id, chatting_id)
+            self.assertEqual(len(get_all_chattings(self.initiator_id, True, db)), 1)
+            self.assertEqual(len(get_all_chattings(self.initiator_id, False, db)), 0)
+
+            with self.assertRaises(InvalidChattingException):
+                terminate_chatting(-1, chatting_id, db)
+            with self.assertRaises(InvalidChattingException):
+                terminate_chatting(self.initiator_id, -1, db)
+            chatting = terminate_chatting(self.initiator_id, chatting_id, db)
+            self.assertEqual(chatting.is_terminated, True)
+            chatting = terminate_chatting(self.responder_id, chatting_id, db)
+            db.commit()
+
+    def test_get_all_texts(self):
+        timestamp = datetime.now()
+
+        for db in DbConnector.get_db():
+            chatting = create_chatting(self.initiator_id, self.responder_id, db)
+            seq_ids = list(db.scalars(insert(Text).values([
+                {"chatting_id": chatting.id, "sender_id": self.initiator_id, "msg": "hello", "timestamp": timestamp},
+                {"chatting_id": chatting.id, "sender_id": self.initiator_id, "msg": "hello", "timestamp": timestamp + timedelta(milliseconds=1)},
+                {"chatting_id": chatting.id, "sender_id": self.initiator_id, "msg": "hello", "timestamp": timestamp + timedelta(milliseconds=2)},
+                {"chatting_id": chatting.id, "sender_id": self.initiator_id, "msg": "hello", "timestamp": timestamp + timedelta(milliseconds=3)},
+                {"chatting_id": chatting.id, "sender_id": self.initiator_id, "msg": "hello", "timestamp": timestamp + timedelta(milliseconds=4)},
+            ]).returning(Text.id)))
+            db.commit()
+
+            self.assertEqual(get_all_texts(-1, chatting.id, -1, None, db), [])
+            self.assertEqual(get_all_texts(-1, None, -1, None, db), [])
+            self.assertEqual(get_all_texts(self.initiator_id, -1, -1, None, db), [])
+            self.assertEqual(len(get_all_texts(self.initiator_id, chatting.id, -1, None, db)), 5)
+            self.assertEqual(len(get_all_texts(self.initiator_id, None, -1, None, db)), 5)
+            self.assertEqual(len(get_all_texts(self.initiator_id, None, seq_ids[1], None, db)), 3)
+
+            texts = get_all_texts(self.initiator_id, None, seq_ids[1], 2, db)
+            self.assertEqual(len(texts), 2)
+            self.assertEqual(texts[0].id, seq_ids[4])
+            self.assertEqual(texts[1].id, seq_ids[3])
 
 
 if __name__ == '__main__':
