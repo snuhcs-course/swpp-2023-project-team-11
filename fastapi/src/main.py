8ae2e78984c091edcd6b339c5b208956e462f0e8
Kangji <drcotr0623@naver.com>
2023-10-29 05:15:18 +0900

    chore: add chatting router to app

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 from src.auth.router import router as auth_router
+from src.chatting.router import router as chatting_router
 from src.user.router import router as user_router
 
 
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(chatting_router)
