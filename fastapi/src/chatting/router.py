3d6cdb6dd3f634bf56d707ff0e1cb9f31c95b446
jioon1830 <jioon1830@gmail.com>
2023-11-12 00:36:53 +0900

    fix: router return more topics

--- fastapi/src/chatting/router.py
+++ fastapi/src/chatting/router.py
@@ -59,11 +59,11 @@ def create_intimacy(chatting_id: int, session: Session = Depends(get_session),
 
 
 @router.get("/topic", response_model=TopicResponse)
-def get_topic_recommendation(chatting_id: int, session: Session = Depends(get_session),
+def get_topic_recommendation(chatting_id: int, limit: int | None = None, session: Session = Depends(get_session),
                              db: DbSession = Depends(DbConnector.get_db)):
     intimacy = service.get_recent_intimacy(session.user_id, chatting_id, db)
     tag = service.get_tag_by_intimacy(intimacy)
-    topic = service.get_topic(tag, db)
+    topic = service.get_topic(tag, limit, db)
     return from_topic(topic)
 
 # # TODO get intimacy endpoint 두 개 다 없애고 get chatting에 Intimacy 추가해서 보내기
