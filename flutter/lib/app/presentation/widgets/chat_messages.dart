63342a7e32e7aea1c25512801911e1607a36aee3
gagaga42 <i4579796868@gmail.com>
2023-12-02 12:52:09 +0900

    feat: 로드맵 채팅 좀 더 예쁘게 나오게

--- flutter/lib/app/presentation/widgets/chat_messages.dart
+++ flutter/lib/app/presentation/widgets/chat_messages.dart
@@ -32,13 +32,11 @@ class ChatMessage extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment:
-          _alignLeft ? MainAxisAlignment.start : MainAxisAlignment.end,
+          _alignLeft && senderType != SenderType.sneki? MainAxisAlignment.start : MainAxisAlignment.end,
       children: [
-        if (_alignLeft && !sameSenderWithBeforeMessage)
+        if (_alignLeft && !sameSenderWithBeforeMessage && senderType != SenderType.sneki)
           CircleAvatar(
-            backgroundImage: (senderType == SenderType.sneki)
-                ? const AssetImage('assets/images/sneki_profile.png')
-                : ProfilePic.call(senderEmail!),
+            backgroundImage: ProfilePic.call(senderEmail!),
             radius: 18.5,
           ),
         if (_alignLeft && sameSenderWithBeforeMessage)
@@ -68,29 +66,54 @@ class ChatMessage extends StatelessWidget {
           ),
         isRoadmapChat
             ? Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 12,
+          margin: const EdgeInsets.symmetric(vertical: 16),
+              padding: const EdgeInsets.only(top: 16, left: 20, right: 16),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
+                gradient: LinearGradient(
+                    stops: [0.015, 0.015],
+                    colors: [MyColor.orange_1, Colors.white]
                 ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: MyColor.orange_1.withOpacity(0.9),
-                ),
-                constraints: const BoxConstraints(maxWidth: 280),
-                child: Text(
+
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.7),
+                    spreadRadius: 0,
+                    blurRadius: 8,
+                    offset: Offset(0, 2),
+                  )
+                ]
+            ),
+            constraints: const BoxConstraints(maxWidth: 330),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Sneki의 추천이 도착했어요!'.tr, style: TextStyle(color: MyColor.textBaseColor, fontSize: 14, fontWeight: FontWeight.w600)),
+                Text('다음 주제에 대해 이야기해보는거 어때요:  '.tr, style: TextStyle(color: MyColor.textBaseColor.withOpacity(0.8), fontSize: 14, fontWeight: FontWeight.w400)),
+                Divider(color: Colors.black.withOpacity(0.1), thickness: 1),
+                Text(
                   // roadmap message format:
                   // ex) "ROADMAP_MESSAGE!Discuss worst movies or dramas"
                   // ex) "ROADMAP_MESSAGE!사회 이슈에 대한 토론" - 영어 한국어 둘 다 와야하는데
                   // - 어차피 유저 설정에 따라서 로드맵 추천으로 나오는 언어가 바뀌는게 나을것같아서 필요할 듯
-                  "${text.replaceFirst(roadmap_prefix, '')}",
+                  "${text}",
                   style: const TextStyle(
-                    fontSize: 14,
-                    color: Color.fromRGBO(45, 58, 69, 0.9),
-                    // fontWeight: FontWeight.w500
+                    fontSize: 16,
+                    color: MyColor.orange_1,
+                    fontWeight: FontWeight.w700
                   ),
                   textAlign: TextAlign.left,
                 ),
-              )
+                Center(
+                  child: Image.asset(
+                    "assets/images/sneki_holding_here.png",
+                    scale: 1.8,
+                  ),
+                ),
+              ],
+            ),
+          )
             : Container(
                 padding: const EdgeInsets.symmetric(
                   vertical: 8,
@@ -98,14 +121,14 @@ class ChatMessage extends StatelessWidget {
                 ),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  color: tempProxy ? Colors.white54 : Colors.white,
+                  color: (senderType == SenderType.me)? (tempProxy ? Color(0xffb085e3) : MyColor.purple) :( tempProxy ? Colors.white54 : Colors.white),
                 ),
                 constraints: const BoxConstraints(maxWidth: 230),
                 child: Text(
                   text,
-                  style: const TextStyle(
+                  style: TextStyle(
                     fontSize: 14,
-                    color: Color.fromRGBO(45, 58, 69, 0.8),
+                    color: (senderType == SenderType.me)? Colors.white : Color.fromRGBO(45, 58, 69, 0.8),
                   ),
                   textAlign: TextAlign.left,
                 ),
